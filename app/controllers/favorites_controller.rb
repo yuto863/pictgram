cf51e8c3dd26d9b4e4b7dac64b8a220bc5100b2a
Yuto Koga <yuuto863koga@yahoo.co.jp>
2019-12-19 06:21:55 +0000

    Finish chapter 21

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -14,4 +14,11 @@ class FavoritesController < ApplicationController
       redirect_to topics_path, danger: 'お気に入りに登録に失敗しました'
     end
   end
+  
+  def destroy
+    Favorite.find_by(params[:topic_id]).destroy
+    flash[:success]="お気に入りを解除しました"
+    redirect_to topics_path
+  end
+  
 end
